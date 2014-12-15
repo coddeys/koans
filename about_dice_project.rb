3ef5492ccc4a3b6a45a8ab9903980576e4921416
Dima Sukhikh <coddeys@gmail.com>
2014-12-15 12:33:35 +0300

    Finish / Mountains are again merely mountains :-)

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -46,8 +46,6 @@ class AboutDiceProject < Neo::Koan
 
     dice.roll(5)
     second_time = dice.values
-    p first_time
-    p second_time
 
     assert_not_equal first_time, second_time,
       "Two rolls should not be equal"
